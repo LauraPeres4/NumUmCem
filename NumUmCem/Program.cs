7b2ffe517ba32da4070dc51e27fbd03f2b678cdb
Usuario <Usuario@CPD02-M16>
2024-03-26 10:48:31 -0300

    Adicionar arquivos de projeto.

---
+++ NumUmCem/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumUmCem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            Console.WriteLine("Informe qual número você deseja saber os múltiplos: ");
+            int num = int.Parse(Console.ReadLine());
+            int cont = num;
+
+
+            while (cont <= 100)
+            {
+                Console.WriteLine(cont);
+                cont += num;
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
